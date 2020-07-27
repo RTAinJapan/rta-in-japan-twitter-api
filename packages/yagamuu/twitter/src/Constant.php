551045a09afec29d9902be443231a1755b871cc2
cma-desktop <yamanaka-team@hotmail.co.jp>
2020-07-27 23:29:47 +0900

    数字の表記を全てのphp7で動くように修正

--- packages/yagamuu/twitter/src/Constant.php
+++ packages/yagamuu/twitter/src/Constant.php
@@ -5,7 +5,7 @@ namespace yagamuu\TwitterClientForRtainjapan;
 
 class Constant
 {
-    const MAX_FILE_SIZE_IMAGE = 5_000_000;
-    const MAX_FILE_SIZE_GIF_ANIME = 15_000_000;
-    const MAX_FILE_SIZE_VIDEO = 512_000_000;
+    const MAX_FILE_SIZE_IMAGE = 5000000;
+    const MAX_FILE_SIZE_GIF_ANIME = 15000000;
+    const MAX_FILE_SIZE_VIDEO = 512000000;
 }
